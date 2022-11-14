8b58585720ae18e3702abde73c6a23c26c0ae1cd
Andrew ZHONG <andrew.zh7204126@gmail.com>
2022-11-14 23:17:41 +0800

    Create mc_config.h

---
+++ mc_config.h
@@ -0,0 +1,11 @@
+#ifndef __MC_Config_H__
+#define __MC_Config_H__
+
+// The syncronous motor including the PMSM motor and the ACIM Motor 
+#define CFG_BLDC_PMSM
+
+// Asyncronous Motor ACIM （三相异步电动机）
+#define CFG_ACIM
+
+
+#endif
